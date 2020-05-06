26079427141ef82e831b20665cf9b1d7676f851b
Andreas Bade <badeand@badeand.com>
2020-05-06 20:39:28 +0200

    Added experiment: Arduino Mega and MAX7219

---
+++ dings01/firmware/cmake-build-esp32doit-devkit-v1/CMakeFiles/firmware.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_BusIO_ID6214/Adafruit_BusIO_Register.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_BusIO_ID6214/Adafruit_I2CDevice.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_BusIO_ID6214/Adafruit_SPIDevice.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_LIS3MDL_ID6931/Adafruit_LIS3MDL.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_LSM9DS1_Library_ID4175/Adafruit_LSM9DS1.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_Unified_Sensor_ID31/Adafruit_Sensor.cpp.o"
+  "CMakeFiles/firmware.dir/.pio/libdeps/esp32doit-devkit-v1/ArduinoOSC_ID1896/examples/apps_to_talk_with_arduino/for_wifi_and_ethernet/oF/src/main.cpp.o"
+  "CMakeFiles/firmware.dir/src/main.cpp.o"
+  "firmware"
+  "firmware.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firmware.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
