eb4df92289b327f41812df7f6e5b0fb4d01b779f
badeand <badeand@badeand.com>
2020-05-06 14:52:42 +0200

    added layout

--- generate.sh
+++ generate.sh
@@ -1,3 +1,11 @@
 plantuml \
   ./dings01/docs/diagrams/components.puml   \
   ./setup/docs/diagrams/ports.puml    \
+
+
+
+rm -rf _layouts
+rm -rf assets
+cp -r ../homepage/_layouts/ _layouts/
+cp -r ../homepage/assets/ assets
+cp ../homepage/_config.yml .
